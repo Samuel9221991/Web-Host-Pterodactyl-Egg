c1b89d50c579dd66e30ba1a6e6f30be212e9d8d6
Samuel9221991 <84253067+Samuel9221991@users.noreply.github.com>
2022-11-18 15:56:13 -0600

    Add files via upload

---
+++ WebHost Egg/install.sh
@@ -0,0 +1,26 @@
+#!/bin/ash
+cd /mnt/server
+apk --update add git
+# cloning repo with nginx and php files
+git clone https://github.com/finnie2006/ptero-nginx ./temp
+cp -r ./temp/nginx /mnt/server/
+cp -r ./temp/php-fpm /mnt/server/
+cp -r ./temp/webroot /mnt/server/
+cp ./temp/start.sh /mnt/server/
+chmod +x /mnt/server/start.sh
+rm -rf ./temp
+mkdir /mnt/server/tmp
+if [ "${WORDPRESS}" == "false" ] || [ "${WORDPRESS}" == "0" ]; then
+echo -e "Install complete go to http://ip:port/"
+fi
+#wordpress
+if [ "${WORDPRESS}" == "true" ] || [ "${WORDPRESS}" == "1" ]; then
+echo -e "Installing wordpress"
+cd /mnt/server/webroot
+wget http://wordpress.org/latest.tar.gz
+tar xzf latest.tar.gz
+mv wordpress/* .
+rm -rf wordpress latest.tar.gz
+echo -e "Install complete go to http://ip:port/wp-admin "
+exit 0
+fi
